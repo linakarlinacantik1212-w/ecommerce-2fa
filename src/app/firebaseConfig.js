b7f07a8c2178d340facdaecf4af9b9bc0fa91be6
linakarlinacantik1212-w <linakarlinacantik1212@gmail.com>
2025-10-18 22:02:29 +0700

    Initial commit - ecommerce 2FA project

--- src/app/firebaseConfig.js
+++ src/app/firebaseConfig.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps, getApp } from "firebase/app";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBQYHj9JIcmceb-5hUK8AWR09t8zSbVasQ",
-  authDomain: "ecommerce-2fa.firebaseapp.com",
-  projectId: "ecommerce-2fa",
-  storageBucket: "ecommerce-2fa.firebasestorage.app",
-  messagingSenderId: "385889115587",
-  appId: "1:385889115587:web:490895c20a02e4ee4a3176",
-  measurementId: "G-S35BYSR887"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Hindari inisialisasi ulang Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export default app;
